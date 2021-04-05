5917e844d7bf500fb08e1deb1c29c72ec8c06fb8
Alice Yang <yangalice212@gmail.com>
2021-04-06 00:47:36 +0800

    Update 2021-04-05T16:47:35.917Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,28 @@
 "use strict";
+
+$(document).ready(function () {
+  $("#removeModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    var title = button.data("title");
+    var modal = $(this);
+    modal.find(".modal-title").text("確認刪除 " + title);
+  });
+}); // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+(function () {
+  "use strict"; // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
+  var forms = document.querySelectorAll(".needs-validation"); // Loop over them and prevent submission
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener("submit", function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add("was-validated");
+    }, false);
+  });
+})();
 //# sourceMappingURL=all.js.map
